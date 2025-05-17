f4a07d512377fec0f6a1655c034e188b6428b4aa
KelvinHenry25 <lamkhang251008@gmail.com>
2025-05-17 13:12:16 +0700

    Update

--- src/rust/vi.truyenqq/build.sh
+++ src/rust/vi.truyenqq/build.sh
@@ -4,3 +4,5 @@ cp res/* target/wasm32-unknown-unknown/release/Payload
 cp target/wasm32-unknown-unknown/release/*.wasm target/wasm32-unknown-unknown/release/Payload/main.wasm
 cd target/wasm32-unknown-unknown/release ; zip -r package.aix Payload
 mv package.aix ../../../package.aix
+mkdir -p ../../../../gh-pages/sources/truyenqq
+cp ../../../package.aix ../../../../gh-pages/sources/truyenqq/
