22a16a67409c25bbb7f43d9aa7bf9673d1eb4d3c
KelvinHenry25 <lamkhang251008@gmail.com>
2025-05-18 23:06:07 +0700

    Update build.sh

--- src/rust/wpcomics/build.sh
+++ src/rust/wpcomics/build.sh
@@ -1,45 +1,37 @@
-#!/bin/bash
-
-set -e  # dừng nếu gặp lỗi
-ROOT_DIR=$(pwd)
-
-build_target="wasm32-unknown-unknown"
+# template source build script
+# usage: ./build.sh [source_name/-a]
 
 if [ "$1" != "-a" ] && [ "$1" != "" ]; then
 	# compile specified source
-	cargo +nightly build --release --target $build_target
+	cargo +nightly build --release
 	
-	echo "Packaging $1"
-	mkdir -p target/$build_target/release/Payload
-	cp res/* target/$build_target/release/Payload
-	if [ -d "sources/$1/res" ]; then
-		cp sources/$1/res/* target/$build_target/release/Payload
-	fi
-	cd target/$build_target/release
-	cp "$1.wasm" Payload/main.wasm
-	zip -r "$1.aix" Payload > /dev/null
-	mv "$1.aix" "$ROOT_DIR/$1.aix"
+	echo "packaging $1";
+	mkdir -p target/wasm32-unknown-unknown/release/Payload
+	cp res/* target/wasm32-unknown-unknown/release/Payload
+	cp sources/$1/res/* target/wasm32-unknown-unknown/release/Payload
+	cd target/wasm32-unknown-unknown/release
+	cp $1.wasm Payload/main.wasm
+	zip -r $1.aix Payload
+	mv $1.aix ../../../$1.aix
 	rm -rf Payload
-	cd "$ROOT_DIR"
 else
 	# compile all sources
-	cargo +nightly build --release --target $build_target
+	cargo +nightly build --release
 
 	for dir in sources/*/
 	do
-		dir=${dir%*/}; dir=${dir##*/}
-		echo "Packaging $dir"
+		dir=${dir%*/}
+		dir=${dir##*/}
+		echo "packaging $dir";
 
-		mkdir -p target/$build_target/release/Payload
-		cp res/* target/$build_target/release/Payload
-		if [ -d "sources/$dir/res" ]; then
-			cp sources/$dir/res/* target/$build_target/release/Payload
-		fi
-		cd target/$build_target/release
-		cp "$dir.wasm" Payload/main.wasm
-		zip -r "$dir.aix" Payload > /dev/null
-		mv "$dir.aix" "$ROOT_DIR/$dir.aix"
+		mkdir -p target/wasm32-unknown-unknown/release/Payload
+		cp res/* target/wasm32-unknown-unknown/release/Payload
+		cp sources/$dir/res/* target/wasm32-unknown-unknown/release/Payload
+		cd target/wasm32-unknown-unknown/release
+		cp $dir.wasm Payload/main.wasm
+		zip -r $dir.aix Payload >> /dev/null
+		mv $dir.aix ../../../$dir.aix
 		rm -rf Payload
-		cd "$ROOT_DIR"
+		cd ../../../
 	done
 fi
